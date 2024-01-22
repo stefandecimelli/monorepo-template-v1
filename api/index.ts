6e42d431de7207af96cc5ec5c4d4566b000e061b
Stefan <decimelli@ibm.com>
2024-01-22 13:04:24 -0500

    watch vite server on api as well

--- api/index.ts
+++ api/index.ts
@@ -1,9 +1,11 @@
 import express from "express";
+import cors from "cors";
 
 const port = process.env.SERVER_PORT || 8080;
 const app = express();
 
 app.use(express.json())
+app.use(cors())
 
 app.get("/api/hello", (_, res) => {
 	res.send({message: "Hello, world!"});
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,24 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import chokidar from 'chokidar';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    {
+      name: 'watch-external',
+      configureServer({ _, ws }) {
+        const apiWatcher = chokidar.watch('./api', {
+          ignoreInitial: true
+        });
+
+        apiWatcher.on('change', () => {
+          ws.send({
+            type: 'full-reload',
+            path: '*'
+          });
+        });
+      }
+    }]
 })
+
